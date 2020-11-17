5f33b980d2d568db599d87328f6316af8efda28d
abakarovmaks <abakarovmaks@gmail.com>
2020-11-17 12:53:23 +0200

    Изменил JS

--- js/task-6.js
+++ js/task-6.js
@@ -1,17 +1,30 @@
-const validationInput = document.querySelector("#validation-input");
+// const validationInput = document.querySelector('#validation-input');
 
-const validationInputLength = function () {
-  if (
-    validationInput.value.length < Number(validationInput.dataset.length) ||
-    validationInput.value.length > Number(validationInput.dataset.length)
-  ) {
-    validationInput.classList.add("invalid");
-  } else {
-    validationInput.classList.add("valid");
+// const validationInputLength = function () {
+//   if (validationInput.value.length === Number(validationInput.dataset.length)) {
+//     validationInput.classList.add('valid');
+//   } else {
+//     validationInput.classList.add('invalid');
+//   }
+// };
+
+// validationInput.addEventListener('focus', () => {
+//   validationInput.classList = '';
+// });
+// validationInput.addEventListener('blur', validationInputLength);
+
+const input = document.querySelector('#validation-input');
+
+const inputBorder = function (event) {
+  input.classList.add('invalid');
+
+  switch (event.currentTarget.value.length) {
+    case Number(event.target.dataset.length):
+      input.classList.replace('invalid', 'valid');
+      break;
+    case 0:
+      input.classList.remove('invalid', 'valid');
+      break;
   }
 };
-
-validationInput.addEventListener("focus", () => {
-  validationInput.classList = "";
-});
-validationInput.addEventListener("blur", validationInputLength);
+input.addEventListener('input', inputBorder);
